8ddcf9b2ec027e21d987c173fe6064ff47e23203
Vaibhav Nagar <vaibhav.nagar@waisl.in>
2023-11-21 16:01:44 +0530

    commit

--- doc_ai/main_vector.py
+++ doc_ai/main_vector.py
@@ -8,9 +8,9 @@ from utils import timer
 def vectorize(data, db="Chroma"):
     embedder = LlamaCppEmbeddings(
         model_path=os.path.abspath("doc_ai/llm/llama-2-7b-32k-instruct.Q4_K_S.gguf"),
-        n_threads=16,
-        n_ctx=16000,
-        verbose=False,
+        n_threads=16,  
+        n_ctx=16000, 
+        verbose=False, 
     )
     if db == "Chroma":
         from vectorizers import chroma_vector
